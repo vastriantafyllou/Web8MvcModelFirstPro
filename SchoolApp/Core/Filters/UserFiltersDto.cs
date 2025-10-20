0e866e4e6ba4ac9e61a66ebf18681539c8e6239a
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-10-20 09:35:04 +0300

    feat: add filters

---
+++ SchoolApp/Core/Filters/UserFiltersDto.cs
@@ -0,0 +1,9 @@
+namespace SchoolApp.Core.Filters
+{
+    public class UserFiltersDto
+    {
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? UserRole { get; set; }
+    }
+}
